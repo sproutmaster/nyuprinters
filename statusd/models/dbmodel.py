086c666d21122daa11b0a92080858838a70b14c0
Joe <joe@osiris.cyber.nyu.edu>
2024-01-02 22:29:08 -0500

    add association table

--- statusd/models/dbmodel.py
+++ statusd/models/dbmodel.py
@@ -1,4 +1,5 @@
 from statusd._statusd import db
+from sqlalchemy.sql import func
 
 
 class Printer(db.Model):
@@ -8,12 +9,13 @@ class Printer(db.Model):
     ip_address = db.Column(db.String(15), nullable=False)
 
     last_status = db.Column(db.Text, nullable=True)
-    last_online = db.Column(db.DateTime, nullable=True)
+    last_online = db.Column(db.DateTime(timezone=True), server_default=func.now())
     last_updated = db.Column(db.DateTime, nullable=True)
 
     display = db.Column(db.Boolean, nullable=False, default=True)
 
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    location_short = db.relationship('Location', backref=db.backref('printers', lazy=True))
 
     def __repr__(self):
         return '<Printer %r>' % self.name
@@ -22,37 +24,19 @@ class Printer(db.Model):
 class Location(db.Model):
     __tablename__ = 'locations'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False, unique=True)
+    name = db.Column(db.String(20), nullable=False)
+    short_name = db.Column(db.String(15), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True)
-
-    printers = db.Column(db.Text)
-
-    def get_printer_ids(self) -> list[int]:
-        return list(map(int, filter(None, self.printers.split(','))))
-
-    def add_printer_id(self, printer_id) -> bool:
-        printer_ids = self.get_printers()
-        if printer_id in printer_ids:
-            return False
-        if len(printer_ids) == 0:
-            self.printers = str(printer_id)
-        else:
-            self.printers += f',{printer_id}'
-        return True
-
-    def reorder_printer_ids(self, printer_ids) -> None:
-        self.printers = ','.join(map(str, printer_ids))
-
-    def remove_printer_id(self, printer_id) -> bool:
-        printer_ids = self.get_printers()
-        if printer_id in printer_ids:
-            printer_ids.remove(printer_id)
-            self.reorder_printers(printer_ids)
-            return True
-        return False
+    printers = db.relationship('Printer', backref=db.backref('locations', lazy=True))
 
     def __repr__(self):
         return '<Locations %r>' % self.name
+
+
+printer_locations = db.Table('printer_locations',
+                             db.Column('printer_id', db.Integer, db.ForeignKey('printers.id')),
+                             db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
+                             )
 
 
 class Settings(db.Model):
