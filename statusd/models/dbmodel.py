3b44232d5f2c2e042759963c7dcdd5ec965753e5
Joe <joe@osiris.cyber.nyu.edu>
2024-01-03 16:03:01 -0500

    update model

--- statusd/models/dbmodel.py
+++ statusd/models/dbmodel.py
@@ -1,51 +1,54 @@
-from statusd._statusd import db
+try:
+    from statusd._statusd import db
+except ModuleNotFoundError:
+    from flask import Flask
+    from flask_sqlalchemy import SQLAlchemy
+
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@localhost:5432/nyup"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = False
+    db = SQLAlchemy(app)
+
 from sqlalchemy.sql import func
-
-
-class Printer(db.Model):
-    __tablename__ = 'printers'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False)
-    ip_address = db.Column(db.String(15), nullable=False)
-
-    last_status = db.Column(db.Text, nullable=True)
-    last_online = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    last_updated = db.Column(db.DateTime, nullable=True)
-
-    display = db.Column(db.Boolean, nullable=False, default=True)
-
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
-    location_short = db.relationship('Location', backref=db.backref('printers', lazy=True))
-
-    def __repr__(self):
-        return '<Printer %r>' % self.name
 
 
 class Location(db.Model):
     __tablename__ = 'locations'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False)
-    short_name = db.Column(db.String(15), nullable=False, unique=True)
-    description = db.Column(db.Text, nullable=True)
-    printers = db.relationship('Printer', backref=db.backref('locations', lazy=True))
+    name = db.Column(db.String(30), nullable=False)
+    short_name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.Text)
+    printers = db.relationship('Printer', backref=db.backref('location'))
 
     def __repr__(self):
         return '<Locations %r>' % self.name
 
 
-printer_locations = db.Table('printer_locations',
-                             db.Column('printer_id', db.Integer, db.ForeignKey('printers.id')),
-                             db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
-                             )
+class Printer(db.Model):
+    __tablename__ = 'printers'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    model = db.Column(db.String(30))
+    ip_address = db.Column(db.String(15), nullable=False)
+    last_status = db.Column(db.Text)
+    last_online = db.Column(db.DateTime(timezone=True))
+    last_updated = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    display = db.Column(db.Boolean, default=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id', ondelete='CASCADE', onupdate='CASCADE'))
+
+    def __repr__(self):
+        return '<Printer %r>' % self.name
 
 
 class Settings(db.Model):
     __tablename__ = 'settings'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    key = db.Column(db.String(20), nullable=False, unique=True)
-    value = db.Column(db.Text, nullable=False)
+    key = db.Column(db.String(25), nullable=False, unique=True)
+    value = db.Column(db.Text)
+    default_value = db.Column(db.Text)
     type = db.Column(db.String(10), nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    description = db.Column(db.Text)
 
     def __repr__(self):
         return '<Settings %r>' % self.key
