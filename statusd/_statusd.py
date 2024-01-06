1f0bbc9bb03f87141030c9d4c143e0026d975b04
Joe <joe@osiris.cyber.nyu.edu>
2024-01-06 00:32:31 -0500

    change db & rm unused imports

--- statusd/_statusd.py
+++ statusd/_statusd.py
@@ -1,13 +1,30 @@
-from sys import path
+#######################################################
+# nyuprinters.com
+#
+# Copyright (c) 2022-2024, Joe Prakash
+# Released under the MIT License
+#######################################################
+
 from flask import Flask
-from urllib.parse import urlparse
-from redis import Redis
-from flask_mongoengine import MongoEngine
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from os import environ
 
-path.append("../")
+
+class Env:
+    def __init__(self):
+        self.discord = environ.get("DISCORD", default='#')
+        self.github = environ.get("GITHUB", default='#')
+        self.postgres_uri = environ.get("POSTGRES_URI", default="postgresql://admin:admin@localhost:5432/nyup")
+
+
+env = Env()
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = env.postgres_uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
-import project_settings
+db = SQLAlchemy(app)
 
-redis_uri_parsed = urlparse(project_settings.redis_uri)
-redis_client = Redis(host=redis_uri_parsed.hostname, port=redis_uri_parsed.port)
+migrate = Migrate(app, db)
