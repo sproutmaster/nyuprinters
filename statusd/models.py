10c393c744b68c73f725971d7e36e6c568979e8c
Joe <joe@osiris.cyber.nyu.edu>
2024-01-07 19:19:47 -0500

    hide sensitive meta for non admin

--- statusd/models.py
+++ statusd/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from pytz import timezone
 from json import loads as json_loads
+
 db = SQLAlchemy()
 
 
@@ -12,6 +13,14 @@ class Location(db.Model):
     short_name = db.Column(db.String(20), nullable=False, unique=True)
     description = db.Column(db.Text)
     printers = db.relationship('Printer', backref=db.backref('location'))
+
+    @property
+    def info(self):
+        return {
+            'name': self.name,
+            'short_name': self.short_name,
+            'description': self.description,
+        }
 
     def __repr__(self):
         return f'<Location {self.id}, {self.name}>'
@@ -30,21 +39,29 @@ class Printer(db.Model):
     ip_address = db.Column(db.String(15), nullable=False)
     current_state = db.Column(db.Text)
     last_state = db.Column(db.Text)
-    last_online = db.Column(db.DateTime())
-    last_updated = db.Column(db.DateTime(), server_default=func.now(), onupdate=func.now())
+    last_online = db.Column(db.DateTime(timezone=True))
+    last_updated = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     display = db.Column(db.Boolean, default=True)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id', ondelete='CASCADE', onupdate='CASCADE'))
 
     @property
-    def info(self):
-        cur_state = json_loads(self.current_state) if self.current_state else ''
-        print(self.last_updated.astimezone(timezone('US/Eastern')))
+    def info(self, admin=False):
+        cur_state = json_loads(self.current_state) if self.current_state else {}
+        meta_from_state = cur_state.get('meta', {})
+
+        if cur_state.get('meta'):
+            del cur_state['meta']
+
+        if not admin and meta_from_state.get('status') == 'success':
+            del meta_from_state['serial']
+            del meta_from_state['ip']
+
         meta = {
             'id': self.id,
             'name': self.name,
-            'location': self.location.short_name,
             'last_online': eastern_time(self.last_online),
             'last_updated': eastern_time(self.last_updated),
+            **meta_from_state,
         }
 
         return {
