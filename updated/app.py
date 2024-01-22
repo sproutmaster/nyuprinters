d0bde2b700bab9145d039bee1a6231bd477d66f2
Joe <joe@osiris.cyber.nyu.edu>
2024-01-21 23:06:17 -0500

    refactor and implement threading

---
+++ updated/app.py
@@ -0,0 +1,73 @@
+import threading
+from time import sleep
+from dataclasses import dataclass
+from os import environ
+import psycopg2
+
+
+@dataclass
+class Env:
+    sourced_url = environ.get("SOURCED_URL", default="http://localhost:8000")
+    postgres_url = environ.get("POSTGRES_URL", default="postgresql://admin:admin@localhost:5432/nyup")
+    db = None
+
+    def __post_init__(self):
+        try:
+            self.db = psycopg2.connect(self.postgres_url)
+        except Exception as e:
+            print(f"Error connecting to database: {e}")
+            exit(1)
+
+
+env = Env()
+
+
+@dataclass
+class Setting:
+    running: bool = False  # updated_run
+    refresh_interval: int = 0  # updated_update_interval
+    session_timeout: int = 0  # updated_try_timout
+    delay: int = 0  # updated_try_delay_factor
+    constant_field_updates: bool = False  # updated_constant_field_updates
+
+
+setting = Setting()
+
+
+class ConfigUpdater:
+    def __init__(self, _setting):
+        self.setting = _setting
+        self.stop_event = threading.Event()
+
+    def update(self):
+        while not self.stop_event.is_set():
+            try:
+                cur = env.db.cursor()
+                cur.execute("SELECT key, value FROM settings WHERE key like 'updated_%'")
+                settings = dict(cur.fetchall())
+                cur.close()
+
+                self.setting.running = bool(settings['updated_run'])
+                self.setting.refresh_interval = int(settings['updated_update_interval'])
+                self.setting.session_timeout = int(settings['updated_try_timout'])
+                self.setting.delay = int(settings['updated_try_delay_factor'])
+                self.setting.constant_field_updates = bool(settings['updated_constant_field_updates'])
+
+                print(f"Running: {self.setting.running}")
+
+                sleep(3)
+
+            except Exception as e:
+                config_updater.stop()
+                print("Exit(1)", e)
+                exit(1)
+
+    def start(self):
+        threading.Thread(target=self.update, daemon=True).start()
+
+    def stop(self):
+        self.stop_event.set()
+
+
+config_updater = ConfigUpdater(setting)
+config_updater.start()
